1a9d8e765b5fcf73a95697853ea2393979481598
Ray Velasquez <rayv156@gmail.com>
2023-03-30 19:18:03 -0500

    edited index

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import Footer from '../components/Footer'
-// import chrisImage from '../../src/images/IMG_1041.png'
 import { faPeopleArrowsLeftRight, faBusinessTime, faCircleCheck } from '@fortawesome/free-solid-svg-icons'
 import { graphql } from "gatsby";
+import vanImage from '../../src/images/dry-van.png';
+import flatbedImage from '../../src/images/flat-bed.png';
+import ltlImage from '../../src/images/ltl-freight.png';
 
 // markup
 const IndexPage = ({data}) => {
@@ -17,60 +19,124 @@ const IndexPage = ({data}) => {
     <main className="parallax">
       <Navbar businessInfo={businessInfo} title={title} description={description}/>
       <Hero businessInfo={businessInfo}/>
-      <div className="container" style={{gap: '1rem'}}>
-      <div className="tile is-ancestor">
-      <div className="tile is-parent">
-    <article className="tile is-child box">
-      <div className="content">
-        <p className="title">What we offer</p>
-        <div className="subtitle">
-       
-        <h1 className="title is-size-4">
-          <FontAwesomeIcon icon={faPeopleArrowsLeftRight} size="lg" />
-          <div>Dry Van - Fully enclosed to protect shipments from outside elements.</div>
-          </h1>
-      
-      <hr></hr>
-        <h1 className="title is-size-4">
-        <FontAwesomeIcon icon={faBusinessTime} size="lg" />
-          <div>Flatbed - Standard flatbeds to specialized equipment, we have you covered.</div>
-          </h1>
-      
-      <hr></hr>
-      
-        <h1 className="title is-size-4">
-        <FontAwesomeIcon icon={faCircleCheck} size="lg" />
-          <div>LTL - Less-than-truckload shipping for smaller parcels.</div>
-          </h1>
-      
-        </div>
-      </div>
-    </article>
-  </div>
-  <div className="tile is-vertical is-6">
-    <div className="tile">
-      <div className="tile is-parent is-vertical">
-        <article className="tile is-child box">
-        <p className="title">Testimonials</p>
-          <p>"Working with RV TRANS these past 2 years and still going has been a pleasure to work with, and the relationship that MAG INTERNATIONAL TRANSPORT LLC has proven growth to our company. RV has great customer service, great leadership, but above all they pay their freight bills on time as to why they have a 100 credit score on DAT load board, that is why moving freight for them is always welcomed.  Thank you Mr. Roy Velasquez for being a valued customer."</p>
-          <p style={{textAlign: 'right'}}>-Javier Morales Jr</p>
-          <p style={{textAlign: 'right'}}>MAG INTERNATIONAL TRANSPORT LLC</p>
-          <p style={{textAlign: 'right'}}>Terminal Manager</p>
-        </article>
-        <article className="tile is-child box">
-          <p>"For the past 3 years we have been working with RV TRANS without any incidents or issues. The Customer Service is excellent. The loads are fairly priced and always paid on time. Their dispatching hours are 24/7, always kind and approachable which in this kind of Industry is definitely a plus."</p>
-          <p style={{textAlign: 'right'}}>-Esther Oviedo</p>
-          <p style={{textAlign: 'right'}}>Imperio Transport, Inc.</p>
-        </article>
-      </div>
-     
-    </div>
-    
-  </div>
+      <div class="content box">
+  <h1>Our Services</h1>
   
+      <div className="columns is-desktop">
+      <div className="column">
+      <div class="card" style={{width: 300, height: 260}}>
+  <div class="card-image">
+    <figure class="image" style={{display: 'flex', justifyContent: 'center'}}>
+      <img src={vanImage} alt="Placeholder image" style={{width: '200px'}}/>
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+        <p class="title is-4">Dry Van</p>
+    </div>
+
+    <div class="content">
+    Fully enclosed to protect shipments from outside elements.
+    </div>
+  </div>
 </div>
-      
-      </div>
+</div>
+<div className="column">
+<div class="card" style={{width: 300, height: 260}}>
+  <div class="card-image">
+    <figure class="image" style={{display: 'flex', justifyContent: 'center'}}>
+      <img src={flatbedImage} alt="Placeholder image" style={{width: '200px'}}/>
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+        <p class="title is-4">Flatbed</p>
+    </div>
+
+    <div class="content">
+    Standard flatbeds to specialized equipment, we have you covered.
+    </div>
+  </div>
+</div>
+</div>
+<div className="column">
+<div class="card" style={{width: 300, height: 260}}>
+  <div class="card-image">
+    <figure class="image" style={{display: 'flex', justifyContent: 'center'}}>
+      <img src={ltlImage} alt="Placeholder image" style={{width: '200px'}}/>
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+        <p class="title is-4">LTL</p>
+    </div>
+
+    <div class="content">
+    Less-than-truckload shipping for smaller parcels.
+    </div>
+  </div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="content box">
+  <h1>What Our Customers Say</h1>
+  
+      <div className="columns is-desktop">
+      <div className="column">
+      <div class="card" style={{width: 350}}>
+  <div class="card-content">
+
+    <p class="title">
+    "As long as they've handled my freight deliveries, my shipments have been picked up and delivered on time without any issue. I recommend you trust the company I'm now proud to call my permanent logistics provider."
+    </p>
+
+    <p class="subtitle">
+      Matt B.
+    </p>
+    <p class="subtitle">
+      Customer
+    </p>
+  </div>
+</div>
+</div>
+<div className="column">
+<div class="card" style={{width: 350}}>
+  <div class="card-content">
+
+    <p class="title">
+    "HUA FREIGHT has easy-to-use software that makes the on-boarding process simple. Uploading documentation and insurance documents was a breeze. Their factoring company was collaborative and received payment in less than 48 hours."
+    </p>
+
+    <p class="subtitle">
+      Neo S.
+    </p>
+    <p class="subtitle">
+      Carrier
+    </p>
+  </div>
+</div>
+</div>
+<div className="column">
+<div class="card" style={{width: 350}}>
+  <div class="card-content">
+
+    <p class="title">
+    "I ship fragile and time-sensitive products to distributors around the county. These guys always make sure my goods reach their destination on time, and I haven't had a single damage claim yet. I wouldn't trust my shipments with anyone else now that I've experienced the best."
+    </p>
+
+    <p class="subtitle">
+      Lisa E.
+    </p>
+    <p class="subtitle">
+      Owner/Operator
+    </p>
+  </div>
+</div>
+</div>
+</div>
+</div>
       <Footer />
     </main>
     </>
